64f331a0c95323ef03112816cc22fe71b7ec1b57
Juan Pablo <jpcc141999@gmail.com>
2022-03-12 11:36:42 -0500

    segundo intento de multiplos en list comprehentions

--- list_comprehentions.py
+++ list_comprehentions.py
@@ -1,10 +1,16 @@
 def run():
-    squares = []
-    for i in range(1, 101):
-        if i**2 % 3 != 0:
-            squares.append(i**2)
-        
-    print(squares)
+    # primer ejemplo de guia para utilizar el for en una lista y agregar estos valores a una lista
+    #squares = []
+    #for i in range(1, 101):
+    #    if i**2 % 3 != 0:
+    #        squares.append(i**2)
+    #    
+    #print(squares)
+    # ejemplo completo de list comprenhentions
+    #squares = [i**2 for i in range(1, 101) if i % 3 != 0]
+    #print(squares)
+    multiples_4_6_9 = [ i % 4 or 6 or 9 for i in range(0,100000) if i % 4 or 6 or 9 != 0]
+    print(multiples_4_6_9)
 
 if __name__ == '__main__':
     run()
