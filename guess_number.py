24fc16306d48361b1df3b885f9e24eb499b76789
Juan Pablo <jpcc141999@gmail.com>
2022-03-17 18:22:23 -0500

    juego de adivinar el numer0

---
+++ guess_number.py
@@ -0,0 +1,37 @@
+import random
+
+
+def run():
+    naturals_numbers = []
+    for i in range(1,101):
+        naturals_numbers.append(i)
+    numero = random.choice(naturals_numbers)
+    
+    print('hola es es el juego de adivina el numero de 1 a 100')
+    print('vamos a comerzar.')
+    answer = int(input('escribe tu numero: '))
+    vida = 5
+    while numero != answer:
+        print(numero)
+        if numero > answer:
+            print('el numero es mas grande')
+            vida = vida - 1
+            print('tienes ' + str(vida) + ' vidas')
+            answer = int(input('escribe un nuevo numero: '))
+            if vida == 0:
+                print('perdiste')
+                break           
+        elif numero < answer:
+            print('el numero es mas pequeño')
+            vida = vida - 1
+            print('tienes ' + str(vida) + ' vidas')
+            answer = int(input('escribe un nuevo numero: '))
+            if vida == 0:
+                print('perdiste')
+                break
+        if answer == numero:
+            print('ganaste')
+
+
+if __name__ == '__main__':
+    run()
